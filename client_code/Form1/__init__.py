4d6338516a4ee5e1e5f110feafb977437436430f
NPS INR Cyber Association <112054161+NPSINRCyberAssociation@users.noreply.github.com>
2022-10-20 13:23:04 +0530

    fix bugs

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -17,24 +17,19 @@ class Form1(Form1Template):
         self.flags_left.text = self.total_flags
 
         # Initialize timer
-        self.end_time = datetime(2022, 10, 18, 15, 40)
+        self.end_time = datetime(2022, 10, 20, 14, 45)
         self.timer_1.interval = 1
 
     def timer_1_tick(self, **event_args):
         '''This method is called Every [interval] seconds. Does not trigger if [interval] is 0.'''
-        remaining_time = (self.end_time - datetime.now()).total_seconds() - 19800
+        remaining_time = (self.end_time - datetime.now()).total_seconds()
         time = datetime.fromtimestamp(remaining_time)
+        print(remaining_time)
         if remaining_time > 3600 * 3:
             self.time_remaining.text = '03:00'
         elif remaining_time < 0:
             self.time_remaining.text = '00:00'
         else:
-            self.time_remaining.text = time.strftime('%M:%S' if remaining_time < 3600 else '%H:%M')
+            #self.time_remaining.text = time.strftime('%M:%S' if remaining_time < 3600 else '%H:%M')
+            self.time_remaining.text = f'{int(remaining_time // 3600)}:{ int((remaining_time % 3600) // 60)}:{int(remaining_time % 60)}'
         self.timer_1.interval = 0 if remaining_time <= 0 else 1
-
-    def leaderboard_refresh_timer_tick(self, **event_args):
-        '''This method is called Every [interval] seconds. Does not trigger if [interval] is 0.'''
-        self.leaderboard_list.items = anvil.server.call('get_leaderboard')
-        self.refresh_data_bindings()
-        self.flags_left.text = str(self.total_flags - sum([r['team_score'] for r in self.leaderboard_list.items]))
-        
