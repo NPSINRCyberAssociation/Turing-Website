06e7213c4333b37756942c456d790b6b535808dd
NPS INR Cyber Association <112054161+NPSINRCyberAssociation@users.noreply.github.com>
2022-10-20 09:35:25 +0530

    fix timer tick

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -24,10 +24,10 @@ class Form1(Form1Template):
         '''This method is called Every [interval] seconds. Does not trigger if [interval] is 0.'''
         remaining_time = (self.end_time - datetime.now()).total_seconds() - 19800
         time = datetime.fromtimestamp(remaining_time)
-        if remaining_time > 3600 * 3:
+        if remaining_time > 3600 * 3 or remaining_time < 0:
             self.time_remaining.text = '00:00'
         else:
-            self.time_remaining.text = time.strftime('%M:%S' if remaining_time > 3600 else '%H:%M')
+            self.time_remaining.text = time.strftime('%M:%S' if remaining_time < 3600 else '%H:%M')
         self.timer_1.interval = 0 if remaining_time <= 0 else 1
 
     def leaderboard_refresh_timer_tick(self, **event_args):
